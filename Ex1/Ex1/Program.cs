bc25e15ec7c1ba7a9c4912beaae2c3a234d44fa2
ManoUPdev <141835220+ManoUPdev@users.noreply.github.com>
2024-08-21 22:15:52 -0300

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a altura do retangulo: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a largura do retangulo: ");
+        double largura = Convert.ToDouble(Console.ReadLine());
+
+        double area = altura * largura;
+
+        Console.WriteLine($"A area do retangulo e: {area}");
+    }
+}
---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void main()
+    {
+       const double taxaDolar = 5.17;
+       const double taxaEuro = 6.14;
+       const double taxaPesoArgentino = 0.05;
+
+       Console.Write("Digite o valor em reais (R$): ");
+       double valorReal = Convert.toDouble(Console.ReadLine());
+
+       double valorDolar = valorReal / taxaDolar;
+       double valorEuro = valorReal / taxaEuro;
+       double valorPesoArgentino = valorReal / taxaPesoArgentino;
+
+       Console.WriteLine($ "Valor em dolares: {valorDolar:F2}");
+       Console.WriteLine($"Valor em euros: {valorEuro:F2}");
+       Console.WriteLine($"Valor em pesos argentinos: {valorPesoArgentino:F2}");
+
+
+    }
+}
+    
+
