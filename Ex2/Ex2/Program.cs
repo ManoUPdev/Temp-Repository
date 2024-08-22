c5dae6cde155f23d44faeee07ec65aa8582938cf
ManoUPdev <141835220+ManoUPdev@users.noreply.github.com>
2024-08-22 11:16:06 -0300

    Add files via upload

--- Ex2/Ex2/Program.cs
+++ Ex2/Ex2/Program.cs
@@ -2,25 +2,21 @@
 
 class Program
 {
-    static void main()
+    static void Main()
     {
-       const double taxaDolar = 5.17;
-       const double taxaEuro = 6.14;
-       const double taxaPesoArgentino = 0.05;
+        const double taxaDolar = 5.17;
+        const double taxaEuro = 6.14;
+        const double taxaPesoArgentino = 0.05;
 
-       Console.Write("Digite o valor em reais (R$): ");
-       double valorReal = Convert.toDouble(Console.ReadLine());
+        Console.Write("Digite o valor em reais (R$): ");
+        double valorReais = Convert.ToDouble(Console.ReadLine());
 
-       double valorDolar = valorReal / taxaDolar;
-       double valorEuro = valorReal / taxaEuro;
-       double valorPesoArgentino = valorReal / taxaPesoArgentino;
+        double valorDolar = valorReais/taxaDolar;
+        double valorEuro = valorReais/taxaEuro;
+        double valorPesoArgentino = valorReais/taxaPesoArgentino;
 
-       Console.WriteLine($ "Valor em dolares: {valorDolar:F2}");
-       Console.WriteLine($"Valor em euros: {valorEuro:F2}");
-       Console.WriteLine($"Valor em pesos argentinos: {valorPesoArgentino:F2}");
-
-
+        Console.WriteLine($"Valor em dolares: {valorDolar:F2}");
+        Console.WriteLine($"Valor em euros:{valorEuro:F2}");
+        Console.WriteLine($"Valor em pesos argentinos:{valorPesoArgentino}");
     }
 }
-    
-
--- Ex3/Ex3/Program.cs
+++ Ex3/Ex3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    Console.Write("Digite o primeiro numero: ");
+    int numero1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Digite o segundo numero: ");
+    int numero2 = Convert.ToInt32(Console.ReadLine());
+
+    if(numero1 > numero2)
+    {
+        Console.WriteLine($"O maior numero e: {numero1}");
+        Console.WriteLine($"O menor numero e: {numero2}");
+    }
+    else if (numero2 > numero1)
+    {
+        Console.WriteLine($"O maior numero e: {numero2}");
+        Console.WriteLine($"O menor numero e: {numero1}");
+    }
+    else
+    {
+        Console.WriteLine("Os numeros sao iguais.");
+    }
+
+    }
+}
