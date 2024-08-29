2f864e9254879cef6a5e8bee6368588aa1204019
ManoUPdev <141835220+ManoUPdev@users.noreply.github.com>
2024-08-28 21:35:09 -0300

    Program.cs
    commit 2

--- Ex5/Ex5/Program.cs
+++ Ex5/Ex5/Program.cs
@@ -1,34 +1,35 @@
-﻿using System;
+using System;
 
 class Program
 {
-   static void Main()
-  {
-    Console.Write("Digite um valor inteiro positivo: ");
-    int valor = Convert.Toint32(Console.ReadLine());
+    static void Main()
+    {
+        
+        Console.Write("Digite um valor inteiro positivo: ");
+        int valor = Convert.ToInt32(Console.ReadLine());
 
-     if(valor < 0)
-     {
-        Console.Write("Por favor, insira um valor positivo.");
-        return;
+        
+        if (valor < 0)
+        {
+            Console.WriteLine("Por favor, insira um valor positivo.");
+            return;
+        }
 
-     }
+        
+        int a = 0, b = 1;
 
-      int a = 0, b = 1;
+        
+        Console.Write(a);
 
+        
+        while (b <= valor)
+        {
+            Console.Write(", " + b);
+            int proximo = a + b;
+            a = b;
+            b = proximo;
+        }
 
-      Console.Write(a);
-
-
-      while(b <= valor)
-      {
-         Console.Write(" , " + b);
-         int proximo = a + b;
-         a = b;
-         b = proximo;
-      }
-
-      Console.Write();
- }
-   
+        Console.WriteLine(); 
+    }
 }
