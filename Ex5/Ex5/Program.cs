8c1359547a7841407c3b57a8f1b8a653d4331400
ManoUPdev <141835220+ManoUPdev@users.noreply.github.com>
2024-08-28 21:27:16 -0300

    Program.cs

--- Ex5/Ex5/Program.cs
+++ Ex5/Ex5/Program.cs
@@ -5,7 +5,30 @@ class Program
    static void Main()
   {
     Console.Write("Digite um valor inteiro positivo: ");
-    int valor = Convert
-  }
+    int valor = Convert.Toint32(Console.ReadLine());
 
+     if(valor < 0)
+     {
+        Console.Write("Por favor, insira um valor positivo.");
+        return;
+
+     }
+
+      int a = 0, b = 1;
+
+
+      Console.Write(a);
+
+
+      while(b <= valor)
+      {
+         Console.Write(" , " + b);
+         int proximo = a + b;
+         a = b;
+         b = proximo;
+      }
+
+      Console.Write();
  }
+   
+}
